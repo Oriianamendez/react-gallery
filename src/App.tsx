131aed9a7addb34fa30365d964edd75011ae4567
Hano2001 <haraldnorlin93@gmail.com>
2024-07-10 11:24:19 +0200

    Gallery component
    Co-authored-by: Orianamendez <egleemendez@gmail.com>
    Co-authored-by: Lukaslaas <lukas.s.laas@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Header } from "./components/header";
 import { Search } from "./components/search";
+import { Gallery } from "./components/gallery";
 
 function App() {
   return (
