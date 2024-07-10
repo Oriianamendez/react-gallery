5f4c0ae49d96c6580a0d59571ae229d552164629
Hano2001 <haraldnorlin93@gmail.com>
2024-07-10 11:06:21 +0200

    Page skeleton
    Co-authored-by: Orianamendez <egleemendez@gmail.com>
    Co-authored-by: Lukaslaas <lukas.s.laas@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>HEADER</header>
+      <main>
+        <form action="">
+          <input type="text" name="" id="" />
+          <button>Search</button>
+        </form>
+        <div>
+          <article>
+            <img src="" alt="image" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum,
+              amet.
+            </p>
+          </article>
+        </div>
+        <button>Prev</button>
+        <button>Next</button>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
