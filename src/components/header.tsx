cd27f7ece848f8b74ec37526c3cdcf8084dc05df
lukas-laas <lukas.s.laas@gmail.com>
2024-07-10 15:25:05 +0200

    update logo
    Co-authored-by: Orianamendez <egleemendez@gmail.com>
    Co-authored-by: Hano2001 <Haraldnorlin93@gmail.com>

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ export const Header = () => {
   return (
     <>
       <header>
-        <img src="" alt="Logo" />
+        <img src="./src/assets/logo.png" alt="Logo" />
         <a href="https://github.com/Oriianamendez/react-gallery">github</a>
       </header>
     </>
