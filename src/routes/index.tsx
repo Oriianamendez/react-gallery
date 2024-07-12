c7e2c01d59701ce2c527aaa577437d1669f318dc
Oriana Mendez <egleemendez@gmail.com>
2024-07-12 10:54:26 +0200

    remove unused lines
    Co-authored-by: Lukas Laas <lukas.s.laas@gmail.com>
    Co-authored-by: Hano2001 <Haraldnorlin93@gmail.com>

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,4 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { Search } from "../components/search";
 
 export const Route = createFileRoute("/")({
   component: () => <h1>LANDING PAGE</h1>,
