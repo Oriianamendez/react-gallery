ac92ded51915ef923ec40b174e1b4e2354c9886d
Oriana Mendez <egleemendez@gmail.com>
2024-07-12 10:53:23 +0200

    logo styling
    Co-authored-by: Lukas Laas <lukas.s.laas@gmail.com>
    Co-authored-by: Hano2001 <Haraldnorlin93@gmail.com>

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ export const Header = () => {
   return (
     <>
       <header>
-        <img src="./src/assets/logo.png" alt="Logo" />
+        <img src="./src/assets/logo.png" alt="Logo" className="logo" />
         <a href="https://github.com/Oriianamendez/react-gallery">github</a>
       </header>
     </>
